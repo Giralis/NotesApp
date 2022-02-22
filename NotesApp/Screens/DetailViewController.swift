3fc6fa61244963460ec17bf8a68a3cd57680c1bc
Vladimir Timofeev <vladimirmail895@gmail.com>
2022-02-22 14:28:51 +0300

    view insets adjust when keyboard appears

--- NotesApp/Screens/DetailViewController.swift
+++ NotesApp/Screens/DetailViewController.swift
@@ -23,6 +23,29 @@ class DetailViewController: UIViewController {
         setNote()
     }
     
+    func setKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            bodyTextView.contentInset = .zero
+        } else {
+            bodyTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame - view.safeAreaInsets.bottom, right: 0)
+        }
+        bodyTextView.scrollIndicatorInsets = bodyTextView.contentInset
+        
+        let selectedRange = bodyTextView.selectedRange
+        bodyTextView.scrollRangeToVisible(selectedRange)
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         setNote()
